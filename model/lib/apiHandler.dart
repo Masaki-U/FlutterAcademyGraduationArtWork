ad7084b877d20f5e186ae2f33bfe2ca4f35fa8a3
内田正輝 <masaki.u@arsaga.jp>
2022-05-14 12:28:03 +0900

    API毎の処理を抽象化する型クラスを作成

--- model/lib/apiHandler.dart
+++ model/lib/apiHandler.dart
@@ -8,16 +8,48 @@ import 'ResponseMovieSearch.dart';
 import 'apiKey.dart';
 
 Future fetchMovieList(Function(ResponseMovieSearch res) action) async {
-  await fetch(baseUrl + "discover/movie?&" + apiKey, (json) {
+  await fetch(baseUrl + "discover/movie?&" + apiKey, ApiContext.common((json) {
     action(ResponseMovieSearch.fromJson(json));
-  });
+  }));
 }
 
-Future fetchMovieDetail(int id, Function(ResponseMovieDetail res) action) async {
-  await fetch(baseUrl + "movie/" + id.toString() + "?&" + apiKey, (json) {
+Future fetchMovieDetail(
+    int id, Function(ResponseMovieDetail res) action) async {
+  await fetch(baseUrl + "movie/" + id.toString() + "?&" + apiKey,
+      ApiContext.common((json) {
     action(ResponseMovieDetail.fromJson(json));
-  });
+  }));
 }
+
+Future fetch(String url, ApiContext apiContext) async {
+  try {
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      print(url + "  APIレスポンス::" + response.body);
+      final parsed = convert.jsonDecode(response.body) as Map<String, dynamic>;
+      apiContext.callback(parsed);
+    }
+  } catch (e) {
+    if (e is SocketException) {
+      print("ネットワークの接続がありません");
+    } else if (e is Exception) {
+      apiContext.fallback(e);
+    }
+  }
+}
+
+class ApiContext {
+  ApiContext(this.callback, this.fallback);
+
+  static ApiContext common(Function(Map<String, dynamic> json) callback) {
+    return ApiContext((json) => {callback(json)}, (p0) => {});
+  }
+
+  final Function(Map<String, dynamic> json) callback;
+  final Function(Exception) fallback;
+}
+
+const baseUrl = "https://api.themoviedb.org/3/";
 
 double checkDouble(dynamic value) {
   if (value is String) {
@@ -28,18 +60,3 @@ double checkDouble(dynamic value) {
     return value;
   }
 }
-
-Future fetch(String url, Function(Map<String, dynamic> json) action) async {
-  try {
-    final response = await http.get(Uri.parse(url));
-    if (response.statusCode == 200) {
-      print(url + "  APIレスポンス::" + response.body);
-      final parsed = convert.jsonDecode(response.body) as Map<String, dynamic>;
-      action(parsed);
-    }
-  } on SocketException {
-    print("ネットワークの接続がありません");
-  }
-}
-
-const baseUrl = "https://api.themoviedb.org/3/";
